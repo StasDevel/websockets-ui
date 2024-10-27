3867ad77d0f18bdaacf7d2e7b27c26fcb9832a60
Stanislav Shkradov <studingDev@gmail.com>
2024-10-27 17:53:14 +0300

    init: start task

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 import { httpServer } from "./src/http_server/index.js";
+import { WebSocketServer } from "ws";
 
 const HTTP_PORT = 8181;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+
+const wss = new WebSocketServer({ port: 3000 });
+wss.on("connection", (ws) => {
+  console.log("connected");
+
+  ws.on("message", (data) => {
+    console.log(data.toString());
+  });
+
+  ws.send("ol");
+  //   ws.close();
+  ws.on("close", () => {
+    console.log("Connection closed");
+  });
+});
