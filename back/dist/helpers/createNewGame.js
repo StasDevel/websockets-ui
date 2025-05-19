79f9f2a7c9a731f9b9bb728ba95277ef250f8c6c
StasDevel <studingDev@gmail.com>
2025-05-19 22:13:26 +0300

    feat: add ts, refactore some code

--- back/dist/helpers/createNewGame.js
+++
@@ -1,8 +0,0 @@
-export default function createNewGame(serverData, passedRoomIndex) {
-    const rooms = serverData.rooms;
-    const gamesObject = rooms[passedRoomIndex].games;
-    const keysOfGames = Object.keys(gamesObject);
-    const gameId = keysOfGames.length > 0 ? Number(keysOfGames[keysOfGames.length - 1]) + 1 : 1;
-    gamesObject[gameId] = {};
-    return String(gameId);
-}
